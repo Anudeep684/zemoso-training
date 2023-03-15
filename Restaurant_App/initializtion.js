56976d9c84b6023eeab3674a58ab39255cd2160e
msa <saianudeepmadasu@gmail.com>
2023-03-15 12:00:23 +0530

    Restaurant App Assignment

---
+++ Restaurant_App/initializtion.js
@@ -0,0 +1,33 @@
+let prices1 = new Map();
+prices1.set("paneerbiriyani", 250);
+prices1.set("mushroombiriyani", 350);
+prices1.set("paneerfriedrice", 450);
+prices1.set("mushroomfriedrice", 260);
+prices1.set("paneermunchurian", 280);
+prices1.set("mushroommunchurian", 240);
+prices1.set("chocolatethickshake", 190);
+prices1.set("vanillathickshake", 100);
+prices1.set("strawberrythickshake", 80);
+prices1.set("butterscotchthickshake", 100);
+prices1.set("chocolatemilkshake", 100);
+prices1.set("vanillamilkshake", 140);
+prices1.set("strawberrymilkshake", 160);
+prices1.set("butterscotchmilkshake", 260);
+
+let tables = [
+  {
+    totalBill: 0,
+    itemCount: 0,
+    items: {},
+  },
+  {
+    totalBill: 0,
+    itemCount: 0,
+    items: {},
+  },
+  {
+    totalBill: 0,
+    itemCount: 0,
+    items: {},
+  },
+];
