514be8d04a2d56dafacff856caff4e7d6ec1dc69
Anudeep684 <123233308+Anudeep684@users.noreply.github.com>
2023-05-10 09:06:32 +0530

    Create Java9.java

---
+++ Java9.java
@@ -0,0 +1,17 @@
+//String and Type Information
+//Using the documentation for java.util.regex.Pattern as a resource, write and test a regular expression that checks a sentence to see 
+//that it begins with a capital letter and ends with a period.
+
+import java.util.regex.Pattern;
+
+public class Java9 {
+    public static void main(String[] args) {
+        String sentence = "I am Anudeep from Zemoso.";
+        String regex = "^[A-Z].*\\.$";
+        if (Pattern.matches(regex, sentence)) {
+            System.out.println("The sentence matches the pattern.");
+        } else {
+            System.out.println("The sentence does not match the pattern.");
+        }
+    }
+}
