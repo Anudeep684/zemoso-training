e7db1012236142135169e1df8580cf1e84e1dac5
Anudeep684 <123233308+Anudeep684@users.noreply.github.com>
2023-05-10 18:25:17 +0530

    Update and rename Java6_3.java to Java6_2.java

--- Java6_2.java
+++ Java6_2.java
@@ -1,14 +1,14 @@
-public class Java6_3 {
+public class Java6_2 {
     private int x;
     private String y;
 
     // First constructor
-    public Java6_3(int x) {
+    public Java6_2(int x) {
         this(x, "default value");
     }
 
     // Second constructor
-    public Java6_3(int x, String y) {
+    public Java6_2(int x, String y) {
         this.x = x;
         this.y = y;
     }
