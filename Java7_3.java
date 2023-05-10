4d16425bd4cd8563b54dd20abbdabf36c16b2918
Anudeep684 <123233308+Anudeep684@users.noreply.github.com>
2023-05-10 18:42:49 +0530

    Update and rename InterfaceDemo_7_3.java to Java7_3.java

--- Java7_3.java
+++ Java7_3.java
@@ -1,3 +1,7 @@
+/*3. Create three interfaces, each with two methods. Inherit a new interface that combines the three, adding a new method. 
+Create a class by implementing the new interface and also inheriting from a concrete class. Now write four methods, each of which takes one of the four
+interfaces as an argument. In main( ), create an object of your class and pass it to each of the methods.
+*/
 // First interface
 interface Interface1 {
     void method1();
@@ -52,7 +56,7 @@ class MyClass extends BaseClass implements CombinedInterface {
     }
 }
 
-public class InterfaceDemo_7_3{
+public class Java7_3{
     // Method that takes an object of any of the four interfaces
     public static void method1(Interface1 obj) {
         obj.method1();
@@ -100,3 +104,17 @@ class BaseClass {
         System.out.println("Base method");
     }
 }
+/*Method 1
+Method 2
+Method 3
+Method 4
+Method 5
+Method 6
+Method 1
+Method 2
+Method 3
+Method 4
+Method 5
+Method 6
+Method 7
+*/
