21fb74fdd215feff8dc91a8a61d38026fdfc7f6b
Anudeep684 <123233308+Anudeep684@users.noreply.github.com>
2023-05-11 10:04:48 +0530

    Update Java3.java

--- Java3.java
+++ Java3.java
@@ -1,6 +1,5 @@
-/*Write a java function that will ping any host ( given as input ) and computes the median of the time taken to ping.
-
-Use the system ping utility, do not use any deprecated methods.
+/*(3)Write a java function that will ping any host ( given as input ) and computes the median of
+     the time taken to ping.Use the system ping utility, do not use any deprecated methods.
 */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
