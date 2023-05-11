02fcce2dc55d081a42eebac13eb2f2a04a2fcd0e
Anudeep684 <123233308+Anudeep684@users.noreply.github.com>
2023-05-11 10:09:48 +0530

    Update Java7_3.java

--- Java7_3.java
+++ Java7_3.java
@@ -1,4 +1,4 @@
-/*3. Create three interfaces, each with two methods. Inherit a new interface that combines the three, adding a new method. 
+/*(7)3. Create three interfaces, each with two methods. Inherit a new interface that combines the three, adding a new method. 
 Create a class by implementing the new interface and also inheriting from a concrete class. Now write four methods, each of which takes one of the four
 interfaces as an argument. In main( ), create an object of your class and pass it to each of the methods.
 */
